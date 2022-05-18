d07efe6c09138ce0efbd2ee31e55355176559757
evertonlaf <evertonlaf@gmail.com>
2022-05-18 20:32:11 -0300

    teste IntelliJ

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Hello! 2");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+    }
+}
