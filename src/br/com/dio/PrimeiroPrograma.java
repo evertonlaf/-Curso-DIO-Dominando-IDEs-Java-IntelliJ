fff9841009c8f79b8040248778d77d08a8d5e280
evertonlaf <evertonlaf@gmail.com>
2022-05-18 20:42:53 -0300

    novo teste IntelliJ

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,9 +2,11 @@ package br.com.dio;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hello! 1");
         System.out.println("Hello! 2");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
+        System.out.println("Hello! 3");
+        System.out.println("Hello! 4");
+
+        System.out.println("atalho sout");
     }
 }
